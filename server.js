93dd5593f45a1b3b951d40a4b80d9a7f35ebcaa4
Tyler Kennedy <tkennedy118@gmail.com>
2020-03-27 18:58:29 -0500

    add handlebar views, build out server

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// handlebars
+app.engine('handlebars', exphbs({ defaultLayout: main}));
+app.set('view engine', 'handlebars');
+
+const routes = require("./controller/burgers_controller.js");
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on PORT " + PORT);
+});
