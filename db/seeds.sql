dbc65d31b7545774d8412e6e363754b05031ca9e
Tyler Kennedy <tkennedy118@gmail.com>
2020-03-27 08:41:51 -0500

    add db folder and basic schema/seeds

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES
+    ("The Madlove Burger", false),
+    ("Smoke & Pepper Burger", false),
+    ("Bacon Cheesburger", false),
+    ("Whiskey River BBQ Burger", false);
