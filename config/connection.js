4eecd7322ab0d3413939a9d9e87a94927b53297d
Tyler Kennedy <tkennedy118@gmail.com>
2020-03-28 00:16:23 -0500

    add jawsdb to environment for deployment to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require('mysql');
+var connection;
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+}
 
 connection.connect(err => {
     if (err) throw err;
